f782971ce6604c9c78ed86533582183398405408
Pravh-01 <pravhjot2004@gmail.com>
2025-10-31 17:14:10 +0530

    onrender didn't work, back to localhost

--- server.js
+++ server.js
@@ -51,5 +51,10 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`✅ Server running on ${PORT}`));
+// const PORT = process.env.PORT || 10000;
+// server.listen(PORT, () => console.log(`✅ Server running on ${PORT}`));
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
-const socket = io("https://dun-chat-server.onrender.com", {
-  transports: ["websocket"],
-});
+// const socket = io("https://dun-chat-server.onrender.com", {
+//   transports: ["websocket"],
+// });
+
+const socket = io("http://localhost:10000");
+
 export default socket;
